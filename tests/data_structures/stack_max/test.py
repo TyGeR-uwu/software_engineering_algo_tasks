0125cd327be07ff4faa87d827986ca95aea0d4bc
Ivan <35425780+TyGeR-uwu@users.noreply.github.com>
2025-12-03 23:29:22 +0300

    Add unit tests for StackMax functionality

---
+++ tests/data_structures/stack_max/test.py
@@ -0,0 +1,20 @@
+def test():
+    stack = StackMax()
+    assert stack.get_max() == "None"
+    assert stack.pop() == "error"
+
+    stack.push(7)
+    assert stack.get_max() == 7
+
+    stack.push(1)
+    stack.push(3)
+    assert stack.get_max() == 7
+
+    stack.pop()
+    stack.pop()
+    assert stack.get_max() == 7
+
+    stack.pop()
+    assert stack.get_max() == "None"
+
+test()
